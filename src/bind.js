fcdc3f2f648e8b90c8f68053a7ca37d297545042
Mike Bostock <mbostock@gmail.com>
2015-12-18 15:52:40 -0800

    Rename curry to bind.

--- src/bind.js
+++ src/bind.js
@@ -1,18 +1,18 @@
 var slice = Array.prototype.slice;
 
-function curry1(type, a) {
+function bind1(type, a) {
   return function(t) {
     return type(t, a);
   };
 }
 
-function curry2(type, a, b) {
+function bind2(type, a, b) {
   return function(t) {
     return type(t, a, b);
   };
 }
 
-function curryN(type, args) {
+function bindN(type, args) {
   args = slice.call(args);
   args[0] = null;
   return function(t) {
@@ -24,8 +24,8 @@ function curryN(type, args) {
 export default function(type, a, b) {
   switch (arguments.length) {
     case 1: return type;
-    case 2: return curry1(type, a);
-    case 3: return curry2(type, a, b);
-    default: return curryN(type, arguments);
+    case 2: return bind1(type, a);
+    case 3: return bind2(type, a, b);
+    default: return bindN(type, arguments);
   }
 };
